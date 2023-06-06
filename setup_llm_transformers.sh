6bc244e27c77002dc09b560a427a0d18870dc794
devonquest <devonquest@proton.me>
2023-06-06 19:56:25 +0200

    integrating venv

--- setup_llm_transformers.sh
+++ setup_llm_transformers.sh
@@ -2,10 +2,9 @@ triton="triton-pre-mlir@git+https://github.com/vchiley/triton.git@triton_pre_mli
 model="mosaicml/mpt-7b-instruct"
 attn_impl="triton"
 
-pip install venv
-pip venv myenv
+apt install python3.10-venv
+python -m venv myenv
 source myenv/bin/activate
-# pip install toolz transformers einops accelerate "$triton"
 
+pip install toolz transformers einops accelerate "$triton"
 # python run_llm_transformers.py --model $model --attn_impl $attn_impl
-pip install toolz
