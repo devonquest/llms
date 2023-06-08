b990ca87ef356f30a83e4ad012e7bfff1fc2fa5b
devonquest <devonquest@proton.me>
2023-06-08 23:53:24 +0200

    attempting to implement new cog_arch idea

--- cog_arch/start_loop.py
+++ cog_arch/start_loop.py
@@ -41,7 +41,7 @@ def setup_pipeline( name, attn_impl ):
 
 def load_prompts():
     with ExitStack() as stack:
-        names = [ "summarize", "improve" ]
+        names = [ "summarize", "compress_4" ]
         return dict(
             zip(
                 names,
