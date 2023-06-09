46af98147d0907528bd5c705adf7b434b5ebbf95
devonquest <devonquest@proton.me>
2023-06-10 01:39:51 +0200

    new gptq version of cog_arch

--- cog_arch_gptq/start_loop.py
+++ cog_arch_gptq/start_loop.py
@@ -125,9 +125,10 @@ def loop_inference( generate, tokenizer ):
 
 tf.logging.set_verbosity( tf.logging.CRITICAL )
 
+import os
+print( os.curdir() )
+
 generate, tokenizer = create_pipeline(
     "TheBloke/gpt4-x-vicuna-13B-GPTQ", "GPT4-x-Vicuna-13B-GPTQ-4bit-128g"
 )
-import os
-print( os.curdir() )
 loop_inference( generate, tokenizer )
