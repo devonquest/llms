df74d21c79f596025606bf5256a36659928722aa
devonquest <devonquest@proton.me>
2023-06-11 00:36:21 +0200

    setting up gptq for cog_arch

--- cog_arch_standard/generate.py
+++ cog_arch_standard/generate.py
@@ -216,7 +216,7 @@ def generate( device, model, tokenizer, input_text ):
             # top_k = 50,
             # temperature = 0.3,
             # repetition_penalty=1.02,
-            # num_beams = 1
+            num_beams = 1
         )
 
     output_text = tokenizer.decode(
