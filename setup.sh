7a93778b62b81198ca31b1ef9a62764e202a482a
devonquest <devonquest@proton.me>
2023-06-10 06:44:56 +0200

    new gptq version of cog_arch

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
-# git lfs install
-# git clone "https://huggingface.co/$model"
+cd /workspace
 
-# apt install -y vim python3.10-venv
-# python -m venv ./
+apt update -y
+apt upgrade -y
 
 wget https://repo.anaconda.com/miniconda/Miniconda3-py310_23.3.1-0-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-py310_23.3.1-0-Linux-x86_64.sh
+export PATH=$PATH:/workspace/miniconda3/bin # repeat after restart
 
-conda create --name gptq python=3.9 -y
+conda create --name gptq python=3.9 pip -y
 conda activate gptq
 conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia
 
@@ -15,25 +15,17 @@ git clone https://github.com/qwopqwop200/GPTQ-for-LLaMa
 cd GPTQ-for-LLaMa
 git checkout fastest-inference-4bit
 
+sed -i "s/safetensors==0.3.0/safetensors==0.3.1/g" requirements.txt
 pip install -r requirements.txt
 python setup_cuda.py install
 
-# for cmd in on off; do
-#     cat << EOF | sed 's/^\s*//' > "$cmd"
-#     #!/bin/bash
+cd /workspace
+wget https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh
+bash ./script.deb.sh
+apt install git-lfs
 
-#     $([ "$cmd" = "on" ] && echo "source ./bin/activate" || echo "deactivate")
-# EOF
+git lfs install
+git clone https://huggingface.co/TheBloke/gpt4-x-vicuna-13B-GPTQ
 
-#     chmod +x "$cmd"
-# done
-
-# source on
-
-# # triton="triton-pre-mlir@git+https://github.com/vchiley/triton.git@triton_pre_mlir_sm90#subdirectory=python"
-# triton="triton"
-# pip install disutils toolz transformers sentencepiece auto-gptq einops accelerate "$triton"
-# # pip install toolz transformers einops accelerate
-
-cd cog_arch_gptq
+cd llms/cog_arch_gptq
 python start_loop.py
