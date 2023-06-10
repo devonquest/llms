3933a3091df76cd8426bdf3c00c0f2d4f12cb14a
devonquest <devonquest@proton.me>
2023-06-10 22:46:28 +0200

    setting up gptq for cog_arch

--- cog_arch_gptq/generate.py
+++ cog_arch_gptq/generate.py
@@ -195,7 +195,7 @@ def generate( device, model, tokenizer, input_text ):
             pad_token_id=tokenizer.pad_token_id,
             # min_length = 400,
             # max_length = 512,
-            max_new_tokens = 200,
+            max_new_tokens = 512,
             top_p = 0.95,
             top_k = 50,
             temperature = 0.8,
