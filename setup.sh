0ae8f9b9aca303ee7f31638afa03dd79ce3b1741
devonquest <devonquest@proton.me>
2023-06-10 03:16:11 +0200

    new gptq version of cog_arch

--- setup.sh
+++ setup.sh
@@ -1,25 +1,39 @@
 # git lfs install
 # git clone "https://huggingface.co/$model"
 
-apt install -y vim python3.10-venv
-python -m venv ./
+# apt install -y vim python3.10-venv
+# python -m venv ./
 
-for cmd in on off; do
-    cat << EOF | sed 's/^\s*//' > "$cmd"
-    #!/bin/bash
+wget https://repo.anaconda.com/miniconda/Miniconda3-py310_23.3.1-0-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
 
-    $([ "$cmd" = "on" ] && echo "source ./bin/activate" || echo "deactivate")
-EOF
+conda create --name gptq python=3.9 -y
+conda activate gptq
+conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia
 
-    chmod +x "$cmd"
-done
+git clone https://github.com/qwopqwop200/GPTQ-for-LLaMa
+cd GPTQ-for-LLaMa
+git checkout fastest-inference-4bit
 
-source on
+pip install -r requirements.txt
+python setup_cuda.py install
 
-# triton="triton-pre-mlir@git+https://github.com/vchiley/triton.git@triton_pre_mlir_sm90#subdirectory=python"
-triton="triton"
-pip install disutils toolz transformers sentencepiece auto-gptq einops accelerate "$triton"
-# pip install toolz transformers einops accelerate
+# for cmd in on off; do
+#     cat << EOF | sed 's/^\s*//' > "$cmd"
+#     #!/bin/bash
+
+#     $([ "$cmd" = "on" ] && echo "source ./bin/activate" || echo "deactivate")
+# EOF
+
+#     chmod +x "$cmd"
+# done
+
+# source on
+
+# # triton="triton-pre-mlir@git+https://github.com/vchiley/triton.git@triton_pre_mlir_sm90#subdirectory=python"
+# triton="triton"
+# pip install disutils toolz transformers sentencepiece auto-gptq einops accelerate "$triton"
+# # pip install toolz transformers einops accelerate
 
 cd cog_arch_gptq
 python start_loop.py
