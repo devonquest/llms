c0e8613deddf60f173ebf30dc03dfcd883dc89ac
devonquest <devonquest@proton.me>
2023-06-10 22:39:48 +0200

    setting up gptq for cog_arch

--- cog_arch_gptq/generate.py
+++ cog_arch_gptq/generate.py
@@ -95,6 +95,8 @@ Summarize the below quoted text in 3 different lengths:
 - 5 words
 - a single word
 
+Text for Instruction 1:
+
 '''
 Tokyo's architecture is a mesmerizing blend of tradition and innovation,
 showcasing the city's unique character. Walking through the bustling streets,
@@ -147,8 +149,32 @@ different lengths:
 - 5 words
 - a single word
 
+Text for Instruction 2:
+
 '''
-nonsense
+In the heart of a bustling metropolis, a captivating tapestry of urban
+life unfolds before your eyes. Majestic skyscrapers reach toward the heavens,
+their sleek facades reflecting the vibrant energy that permeates the streets
+below. The rhythm of life pulses through every corner, as a kaleidoscope of
+diverse cultures intertwines, creating a vibrant mosaic of human experiences.
+Sidewalks teem with a symphony of sounds, from the chatter of passersby to
+the enticing melodies spilling out from cozy cafes and lively music venues.
+The air is alive with the aromas of world cuisines, wafting from bustling food
+carts and acclaimed restaurants, offering a tantalizing culinary journey for
+adventurous palates. Vibrant street art adorns the walls, transforming once
+ordinary spaces into canvases of creativity, telling stories of passion,
+resilience, and the human spirit. Parks and green spaces provide a sanctuary
+amidst the urban bustle, inviting city dwellers to reconnect with nature and
+find solace in the midst of the concrete jungle. Technology hums in the
+background, driving innovation and connecting people in ways never before
+imagined. Markets bustle with activity, where the vibrant colors of fresh
+produce, the intricate craftsmanship of local artisans, and the friendly
+banter of vendors create a sensory feast for visitors and locals alike. In
+this vibrant tapestry of urban life, dreams take flight, connections are
+forged, and the city's pulse beats with endless possibilities. It is a place
+where the past and the future converge, inviting all who enter to embrace the
+energy, seize the opportunities, and create their own unique story within the
+vibrant embrace of the metropolis.
 '''
 
 ### Response 2:"""
