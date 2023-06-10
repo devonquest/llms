dc280e4999789c4d5afeb4dd5ee1a36f47639506
devonquest <devonquest@proton.me>
2023-06-10 08:11:48 +0200

    new gptq version of cog_arch

--- cog_arch_gptq/start_loop.py
+++ cog_arch_gptq/start_loop.py
@@ -110,7 +110,7 @@ model = lm.load_quant(
     ".compat.act-order.safetensors",
     4,
     128,
-    1
+    "cuda:0"
 ).to( device )
 tokenizer = tf.AutoTokenizer.from_pretrained( model_dir, use_fast = False )
 
