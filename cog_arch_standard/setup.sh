89b204fe89e1c2071a9671a4998d57a26311099d
devonquest <devonquest@proton.me>
2023-06-10 23:50:34 +0200

    setting up gptq for cog_arch

--- cog_arch_standard/setup.sh
+++ cog_arch_standard/setup.sh
@@ -8,9 +8,9 @@ bash Miniconda3-py310_23.3.1-0-Linux-x86_64.sh -b -p /workspace/miniconda3
 # repeat after restart
 export PATH=$PATH:/workspace/miniconda3/bin
 
-conda create --name gptq python=3.10 pip -y
+conda create --name cog_arch python=3.10 pip -y
 source /workspace/miniconda3/etc/profile.d/conda.sh
-conda activate gptq
+conda activate cog_arch
 
 conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia
 conda install triton transformers
