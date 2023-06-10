923971ac67897e134f778bcefbfb0ce5cb09de7f
devonquest <devonquest@proton.me>
2023-06-10 02:23:37 +0200

    new gptq version of cog_arch

--- setup.sh
+++ setup.sh
@@ -19,3 +19,6 @@ source on
 triton="triton-pre-mlir@git+https://github.com/vchiley/triton.git@triton_pre_mlir_sm90#subdirectory=python"
 pip install disutils toolz transformers sentencepiece auto-gptq einops accelerate "$triton"
 # pip install toolz transformers einops accelerate
+
+cd cog_arch_gptq
+python start_loop.py
