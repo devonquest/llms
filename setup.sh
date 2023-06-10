fbbe5f25b78e9420935d59d56f3348e5c564d317
devonquest <devonquest@proton.me>
2023-06-10 21:59:58 +0200

    setting up gptq for cog_arch

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ apt update -y
 apt upgrade -y
 
 wget https://repo.anaconda.com/miniconda/Miniconda3-py310_23.3.1-0-Linux-x86_64.sh
-bash Miniconda3-py310_23.3.1-0-Linux-x86_64.sh
+bash Miniconda3-py310_23.3.1-0-Linux-x86_64.sh -b -p /workspace/miniconda3
 # repeat after restart
 export PATH=$PATH:/workspace/miniconda3/bin
 
