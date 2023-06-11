67641badf8afc432288c58656cd1910140cfdb4a
devonquest <devonquest@proton.me>
2023-06-11 23:53:33 +0200

    setting up gptq for cog_arch

--- cog_arch_mpt_7b/setup.sh
+++ cog_arch_mpt_7b/setup.sh
@@ -11,6 +11,8 @@ export PATH=$PATH:/workspace/miniconda3/bin
 conda create --name mpt-7b python=3.9 pip -y
 source /workspace/miniconda3/etc/profile.d/conda.sh
 conda activate mpt-7b
+
+conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia -y
 yes | pip install transformers einops
 
 cd /workspace/llms/cog_arch_mpt_7b
