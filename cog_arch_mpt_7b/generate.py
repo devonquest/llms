4ae8e1f00101e6d965dde7f74960748ac2133332
devonquest <devonquest@proton.me>
2023-06-13 01:16:28 +0200

    attempting cog_arch with mpt-7b

--- cog_arch_mpt_7b/generate.py
+++ cog_arch_mpt_7b/generate.py
@@ -17,6 +17,9 @@ def generate( model, tokenizer, device, prompts ):
 
     generated_ids = generated_ids[ 0 ]
     items = [ el.item() for el in generated_ids ]
-    output_text = tokenizer.decode( items )
+    # output_text = tokenizer.decode( items )
     
-    return output_text.replace( used_input_text, "" ).strip()
+    # return output_text.replace( used_input_text, "" ).strip()
+    output_text = tokenizer.decode( items )
+    print( output_text )
+    return ""
