1c19d11860f7faad7325327675a2b31b722e9641
devonquest <devonquest@proton.me>
2023-06-14 00:52:16 +0200

    attempting cog_arch in gptq again

--- cog_arch_autogptq/setup.sh
+++ cog_arch_autogptq/setup.sh
@@ -21,17 +21,12 @@ conda create -p cog_arch python=3.9 pip -y
 source /workspace/miniconda3/etc/profile.d/conda.sh
 conda activate cog_arch
 
+conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia -y
+
 cd /workspace
 git clone https://github.com/PanQiWei/AutoGPTQ.git
 cd AutoGPTQ
-pip install .
-# pip install .[triton]
-
-# conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia -y
-# yes | pip install auto-gptq[triton]
-
-# conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia -y
-# yes | pip install auto-gptq
+yes | pip install .
 
 cd /workspace/llms/cog_arch_autogptq
 python start_loop.py
