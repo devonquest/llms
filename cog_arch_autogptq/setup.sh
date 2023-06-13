70b90e1ebbf8f480545e3f929aaf85628801ed71
devonquest <devonquest@proton.me>
2023-06-14 00:03:08 +0200

    attempting cog_arch in gptq again

--- cog_arch_autogptq/setup.sh
+++ cog_arch_autogptq/setup.sh
@@ -9,13 +9,14 @@ bash Miniconda3-py310_23.3.1-0-Linux-x86_64.sh -b -p /workspace/miniconda3
 export PATH=$PATH:/workspace/miniconda3/bin
 
 cd /workspace/miniconda3/envs
-conda create --prefix cog_arch python=3.9 pip -y
+conda create -p cog_arch python=3.9 pip -y
+
 source /workspace/miniconda3/etc/profile.d/conda.sh
 conda activate cog_arch
 
 conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia -y
-conda install chardet cchardet -y
-yes | pip install chardet auto-gptq[triton]
+# conda install chardet cchardet -y
+yes | pip install auto-gptq[triton]
 
 cd /workspace/llms/cog_arch_autogptq
 python start_loop.py
