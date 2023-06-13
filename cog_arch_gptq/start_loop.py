e60ed5236c1ad94d0b60456bba95c31b84ca6ed9
devonquest <devonquest@proton.me>
2023-06-13 22:59:57 +0200

    attempting cog_arch in gptq again

--- cog_arch_gptq/start_loop.py
+++ cog_arch_gptq/start_loop.py
@@ -101,8 +101,8 @@ model = lm.load_quant(
     model_dir,
     f"{ model_dir }/Wizard-Vicuna-13B-Uncensored-GPTQ-4bit-128g.compat.no-act-order.safetensors",
     4,
-    None
-    # act_order = False
+    -1,
+    act_order = False
 ).to( device )
 # model = lm.load_quant(
 #     model_dir,
