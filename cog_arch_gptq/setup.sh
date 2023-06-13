f2594db4b716761dcf9dd0813e1c876ffc49f1d8
devonquest <devonquest@proton.me>
2023-06-13 22:46:53 +0200

    attempting cog_arch in gptq again

--- cog_arch_gptq/setup.sh
+++ cog_arch_gptq/setup.sh
@@ -1,7 +1,7 @@
 cd /workspace
 
 apt update -y
-apt upgrade -y
+# apt upgrade -y
 
 wget https://repo.anaconda.com/miniconda/Miniconda3-py310_23.3.1-0-Linux-x86_64.sh
 bash Miniconda3-py310_23.3.1-0-Linux-x86_64.sh -b -p /workspace/miniconda3
