ae09d1616d90575318a0df34376e914f6146b3ff
devonquest <devonquest@proton.me>
2023-06-14 02:23:31 +0200

    attempting cog_arch in gptq again

--- cog_arch_gptq/generate.py
+++ cog_arch_gptq/generate.py
@@ -2,18 +2,19 @@ import torch as to
 
 def generate( device, model, tokenizer, prompts ):
     # used_input_text = prompts[ "summarize" ]
-    used_input_text = """Within this decade, AI will become a ubiquitous tool for businesses, transforming the way they operate and compete. It will enable companies to automate routine tasks, optimize processes, and make better decisions based on data and insights. However, AI also raises significant challenges, including the need to manage and govern data, ensure transparency and accountability, and address potential job displacement and societal impacts. To harness the potential of AI while mitigating its risks, businesses and governments must work together to establish a framework for responsible AI. This framework should include principles for fairness, transparency, accountability, and inclusivity,as well as mechanisms for oversight, regulation, and collaboration. By working together, we can ensure that AI delivers on its promise to improve our lives and our world. Furthermore, """
-#     used_input_text = """### Instruction:
+    used_input_text = """Within this decade, AI will become a ubiquitous tool for businesses,
+transforming the way they operate and compete. It will enable companies to
+automate routine tasks, optimize processes, and make better decisions based on
+data and insights. However, AI also raises significant challenges, including
+the need to manage and govern data, ensure transparency and accountability, and
+address potential job displacement and societal impacts. To harness the
+potential of AI while mitigating its risks, businesses and governments must
+work together to establish a framework for responsible AI. This framework
+should include principles for fairness, transparency, accountability, and
+inclusivity,as well as mechanisms for oversight, regulation, and collaboration.
+By working together, we can ensure that AI delivers on its promise to improve
+our lives and our world. Furthermore, """
 
-# Summarize the input text in 3 different lengths:
-
-# - 30 words
-
-# ### Input:
-
-# The art of culinary excellence.
-
-# ### Response:"""
     input_ids = tokenizer.encode( used_input_text, return_tensors = "pt" ) \
         .to( device )
 
@@ -25,7 +26,7 @@ def generate( device, model, tokenizer, prompts ):
             pad_token_id = tokenizer.pad_token_id,
             # min_length = 400,
             # max_length = 512,
-            max_new_tokens = 100,
+            max_new_tokens = 300,
             top_p = 0.1,
             top_k = 50,
             temperature = 0.1,
