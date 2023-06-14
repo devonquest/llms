e0c91573b1acc19828bbaeb9dbd98f5ad8b52cb1
devonquest <devonquest@proton.me>
2023-06-14 03:06:20 +0200

    attempting cog_arch in gptq again

--- cog_arch_gptq/generate.py
+++ cog_arch_gptq/generate.py
@@ -14,7 +14,7 @@ def generate( device, model, tokenizer, prompts ):
             do_sample = True,
             eos_token_id = tokenizer.eos_token_id,
             pad_token_id = tokenizer.pad_token_id,
-            max_new_tokens = 300,
+            # max_new_tokens = 300,
             top_p = 0.1,
             top_k = 50,
             temperature = 0.1,
