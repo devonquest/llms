f1e0a33a059fbb81e580750ae0857bf8f7cb6c62
devonquest <devonquest@proton.me>
2023-06-14 02:17:36 +0200

    attempting cog_arch in gptq again

--- cog_arch_gptq/generate.py
+++ cog_arch_gptq/generate.py
@@ -20,16 +20,16 @@ The art of culinary excellence.
     with to.no_grad():
         generated_ids = model.generate(
             input_ids,
-            use_cache=True, do_sample=True,
-            eos_token_id=tokenizer.eos_token_id,
-            pad_token_id=tokenizer.pad_token_id,
+            use_cache = True, do_sample = True,
+            eos_token_id = tokenizer.eos_token_id,
+            pad_token_id = tokenizer.pad_token_id,
             # min_length = 400,
             # max_length = 512,
             max_new_tokens = 50,
             top_p = 0.1,
             top_k = 50,
             temperature = 0.1,
-            repetition_penalty=1.02
+            # repetition_penalty = 1.02
         )
 
     output_text = tokenizer.decode( [ el.item() for el in generated_ids[ 0 ] ] )
